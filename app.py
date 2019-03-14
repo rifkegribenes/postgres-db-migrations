c21cde273f3b2c73d7a95313f2797a4d3d19cee0
rifkegribenes <rifkegribenes@gmail.com>
2019-03-13 21:58:10 -0700

    initialize db

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from marshmallow import ValidationError
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 
 from db import db
@@ -22,6 +23,7 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "jose"
 api = Api(app)
 jwt = JWTManager(app)
+migrate = Migrate(app, db)
 
 
 @app.before_first_request
